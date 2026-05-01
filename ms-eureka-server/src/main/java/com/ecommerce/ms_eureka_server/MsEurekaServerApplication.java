be24b6bba075fbef7f82c3dcc977895a8505133b
space69-six <martindrew401@gmail.com>
2026-04-30 19:11:16 -0600

    feat: agregar ms-eureka-server

---
+++ ms-eureka-server/src/main/java/com/ecommerce/ms_eureka_server/MsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ms_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MsEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaServerApplication.class, args);
+    }
+}
---
+++ ms-eureka-server/src/test/java/com/ecommerce/ms_eureka_server/MsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ms_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
